8d5b0a0189e5690b9cda69cf86e94fff2a3eb3b1
TSPU\student <mamay3223@rambler.ru>
2023-04-22 15:14:51 +0300

    lab 1

---
+++ api/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tspuWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("{count}")]
+        public int[] GetRandomNumbers(int count)
+        {
+            Random random = new Random();
+            int[] randomNumbers = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                randomNumbers[i] = random.Next(0, 100);
+            }
+
+            return randomNumbers;
+        }
+
+    }
+}
