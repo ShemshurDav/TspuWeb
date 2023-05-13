4b701318b676cb0d0cf4fced2ead0ab4277941b6
TSPU\student <mamay3223@rambler.ru>
2023-05-13 14:28:06 +0300

    Revert "Rep + Controller"
    This reverts commit 3a14c0c61d998986786956e61d6a332658db0059.

--- api/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace tspuWeb.Models
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string? UserName { get; set; }
-        public string? Login { get; set; }
-    }
-}
