3a14c0c61d998986786956e61d6a332658db0059
TSPU\student <mamay3223@rambler.ru>
2023-05-13 14:27:04 +0300

    Rep + Controller

---
+++ api/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace tspuWeb.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? UserName { get; set; }
+        public string? Login { get; set; }
+    }
+}
